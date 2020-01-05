10c4e80ac1844eeb7c3576b07ab51dc0049fdbe5
ricksr <sutirtharej@gmail.com>
2020-01-05 17:53:14 +0530

    code for cmd inputs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let request = require('request');
+const argv = require('yargs').argv;
 
 let apiKey = '3240b1e75c8dd9330997742e3b7fcedb';
-let city = 'kolkata';
+let city = argv.c || 'kolkata';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 request(url, function(err, response, body){
