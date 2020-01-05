ee5c97d544a24f7161217d80349ad9249456ad8f
ricksr <sutirtharej@gmail.com>
2020-01-06 02:10:40 +0530

    html added in index.ejs and unit changes to metric

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
+const request = require('request');
+const apiKey = '3240b1e75c8dd9330997742e3b7fcedb';
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+    res.render('index');
 })
+
 app.post('/', function (req, res) {
-  console.log(req.body.city);
-  res.render('index');  
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    request(url, function (err, response, body) {
+        if (err) {
+            res.render('index', {
+                weather: null,
+                error: 'Error, please try again'
+            });
+        } else {
+            let weather = JSON.parse(body)
+            if (weather.main == undefined) {
+                res.render('index', {
+                    weather: null,
+                    error: 'Error, please try again'
+                });
+            } else {
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', {
+                    weather: weatherText,
+                    error: null
+                });
+            }
+        }
+    });
 })
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
