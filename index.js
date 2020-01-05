015f99cb688aa9f12b4825dd8a3915061de71461
ricksr <sutirtharej@gmail.com>
2020-01-05 17:47:48 +0530

    temp issue fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let request = require('request');
 
 let apiKey = '3240b1e75c8dd9330997742e3b7fcedb';
 let city = 'kolkata';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 request(url, function(err, response, body){
     if(err) {
