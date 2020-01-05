88fe0229545927067eea5e646942d55ec6db76cd
ricksr <sutirtharej@gmail.com>
2020-01-06 01:45:28 +0530

    post method added

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express()
+const bodyParser = require('body-parser');
 
+app.unsubscribe(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.render('index');
+})
+
+app.post('/', function (req, res) {
+    res.render('index');
+    console.log(req.body.city);
 })
 
 app.listen(3000, function () {
